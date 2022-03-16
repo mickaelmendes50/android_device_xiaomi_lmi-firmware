f7ba537e9b49049756eb27c6c5d5a24b8327f2e5
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-03-16 07:48:36 -0500

    lmi: firmware: Switch to seperate directory for firmware
    Signed-off-by: Mesquita <mesquita@aospa.co>

---
+++ firmware.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES += \
+        device/xiaomi/lmi-firmware/images/logo.img:install/firmware-update/logo.img \
+        device/xiaomi/lmi-firmware/images/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+        device/xiaomi/lmi-firmware/images/xbl_config_5.elf:install/firmware-update/xbl_config_5.elf \
+        device/xiaomi/lmi-firmware/images/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+        device/xiaomi/lmi-firmware/images/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+        device/xiaomi/lmi-firmware/images/BTFM.bin:install/firmware-update/BTFM.bin \
+        device/xiaomi/lmi-firmware/images/km4.mbn:install/firmware-update/km4.mbn \
+        device/xiaomi/lmi-firmware/images/xbl_5.elf:install/firmware-update/xbl_5.elf \
+        device/xiaomi/lmi-firmware/images/tz.mbn:install/firmware-update/tz.mbn \
+        device/xiaomi/lmi-firmware/images/aop.mbn:install/firmware-update/aop.mbn \
+        device/xiaomi/lmi-firmware/images/featenabler.mbn:install/firmware-update/featenabler.mbn \
+        device/xiaomi/lmi-firmware/images/xbl_config_4.elf:install/firmware-update/xbl_config_4.elf \
+        device/xiaomi/lmi-firmware/images/storsec.mbn:install/firmware-update/storsec.mbn \
+        device/xiaomi/lmi-firmware/images/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+        device/xiaomi/lmi-firmware/images/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+        device/xiaomi/lmi-firmware/images/abl.elf:install/firmware-update/abl.elf \
+        device/xiaomi/lmi-firmware/images/dspso.bin:install/firmware-update/dspso.bin \
+        device/xiaomi/lmi-firmware/images/devcfg.mbn:install/firmware-update//devcfg.mbn \
+        device/xiaomi/lmi-firmware/images/xbl_4.elf:install/firmware-update/xbl_4.elf \
+        device/xiaomi/lmi-firmware/images/hyp.mbn:install/firmware-update/hyp.mbn \
+        device/xiaomi/lmi-firmware/images/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+        device/xiaomi/lmi-firmware/images/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+        device/xiaomi/lmi-firmware/images/tz.mbn:install/firmware-update/tz.mbn \
+        device/xiaomi/lmi-firmware/images/aop.mbn:install/firmware-update/aop.mbn \
+        device/xiaomi/lmi-firmware/images/storsec.mbn:install/firmware-update/storsec.mbn \
+        device/xiaomi/lmi-firmware/images/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+        device/xiaomi/lmi-firmware/images/abl.elf:install/firmware-update/abl.elf \
+        device/xiaomi/lmi-firmware/images/devcfg.mbn:install/firmware-update/devcfg.mbn \
+        device/xiaomi/lmi-firmware/images/hyp.mbn:install/firmware-update/hyp.mbn
